fd651c059cd1c720945e57a2518e5db06e764c38
chx <chx7@live.com>
2022-06-12 18:00:25 +0800

    ch2.6, diesel not wokring so far

---
+++ ch2/dieselserver.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import diesel
+import argparse
+# import logging
+# logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+
+class EchoServer(object):
+    '''
+    A echo server using diesel
+    '''
+
+    def handler(self, remote_addr):
+        '''
+        run the echo server
+        '''
+        host, port = remote_addr[0], remote_addr[1]
+        print('Echo client connected from: {}:{}'.format(host, port))
+
+        while True:
+            try:
+                message = diesel.until_eol()
+                your_msg = ': '.join(['You said', message])
+                diesel.send(your_msg)
+            except Exception as e:
+                print('Exception: {}'.format(e))
+
+
+def main(server_port):
+    app = diesel.Application()
+    server = EchoServer()
+    app.add_service(diesel.Service(server.handler, server_port))
+    app.run()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Echo server with Diesel')
+    parser.add_argument(
+        '--port',
+        action='store',
+        dest='port',
+        type=int,
+        required=True)
+    args = parser.parse_args()
+    main(args.port)
